3387bb16d224d67411ac16b808fee6e8e6582e9f
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-09-25 21:27:59 +0900

    add: 구조체 정의

--- cub3d.h
+++ cub3d.h
@@ -3,18 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dakang <dakang@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 19:30:26 by dakang            #+#    #+#             */
-/*   Updated: 2024/09/23 19:33:49 by dakang           ###   ########.fr       */
+/*   Updated: 2024/09/25 21:09:08 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+typedef struct s_player
+{
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+}	t_player;
+
+typedef struct s_cub
+{
+	int			fd;
+	char		*map;
+	char		**map_arr;
+	void		*mlx;
+	void		*win;
+	int			player_num;
+	t_player	*player;
+}	t_cub;
 
 #endif
