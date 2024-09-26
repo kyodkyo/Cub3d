7b9c7c261be90d3e8f2469ee7ecef349fa6f20aa
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-09-26 20:29:01 +0900

    refactor: player struct에 평면 방향 추가

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 19:30:26 by dakang            #+#    #+#             */
-/*   Updated: 2024/09/25 21:09:08 by dakyo            ###   ########.fr       */
+/*   Updated: 2024/09/26 20:01:31 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@
 # include <stdlib.h>
 # include <fcntl.h>
 
+# include "mlx/mlx.h"
+# include "get_next_line.h"
+
 typedef struct s_player
 {
 	double	pos_x;
 	double	pos_y;
 	double	dir_x;
 	double	dir_y;
+	double	plane_x; // 카메라 평면의 X축 좌표
+	double	plane_y; // 카메라 평면의 Y축 좌표
 }	t_player;
 
 typedef struct s_cub
