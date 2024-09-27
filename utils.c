dad2debe59c306ef80cb929ae053708fef812532
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-09-27 13:10:04 +0900

    feat: valid value check

--- utils.c
+++ utils.c
@@ -6,11 +6,11 @@
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 20:34:06 by dakyo             #+#    #+#             */
-/*   Updated: 2024/09/25 20:40:15 by dakyo            ###   ########.fr       */
+/*   Updated: 2024/09/27 12:54:01 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	free_2d(char **str, int flag)
+int	free_2d_arr(char **str, int flag)
 {
 	int	i;
 
@@ -19,4 +19,18 @@ int	free_2d(char **str, int flag)
 		free(str[i++]);
 	free(str);
 	return (flag);
+}
+
+int	is_space(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] != ' ' && line[i] != '\t' && line[i] != '\n')
+			return (0);
+		i++;
+	}
+	return (1);
 }
--- valid_check.c
+++ valid_check.c
@@ -6,7 +6,7 @@
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 19:53:42 by dakyo             #+#    #+#             */
-/*   Updated: 2024/09/26 20:27:03 by dakyo            ###   ########.fr       */
+/*   Updated: 2024/09/27 13:05:45 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,27 +26,22 @@ int	check_file(char *str)
 		count++;
 	len = ft_strlen(tmp[count]);
 	if (len <= 4)
-		return (free_2d(tmp, 1));
+		return (free_2d_arr(tmp, 1));
 	if (!(tmp[count][len - 4] == '.' && tmp[count][len - 3] == 'c'
 		&& tmp[count][len - 2] == 'u' && tmp[count][len - 1] == 'b'))
-		return (free_2d(tmp, 1));
-	return (free_2d(tmp, 0));
+		return (free_2d_arr(tmp, 1));
+	return (free_2d_arr(tmp, 0));
 }
 
-int	is_space(char *line)
+void	check_dir()
 {
-	int	i;
-
-	i = 0;
-	while (line[i])
-	{
-		if (line[i] != ' ' && line[i] != '\t' && line[i] != '\n')
-			return (0);
-		i++;
-	}
-	return (1);
+	
 }
 
+void check_color()
+{
+	
+}
 
 void	dir_color_check(char *line, int *count, t_cub *cub)
 {
@@ -55,16 +50,51 @@ void	dir_color_check(char *line, int *count, t_cub *cub)
 	if (line[0] == '\n' && !(line[1]))
 		return ;
 	if (is_space(line))
-		printf("is space error\n");
+		printf("only space error\n");
 	tmp = ft_split(line, ' ');
 	if (!tmp)
 		printf("split error\n");
-	//dir : EA, WE, SO, NO
-	//color : F, C
-	//free
+	if (!(ft_strncmp(tmp[0], "EA", 2)) || !(ft_strncmp(tmp[0], "WE", 2))
+		|| !(ft_strncmp(tmp[0], "SO", 2)) || !(ft_strncmp(tmp[0], "NO", 2)))
+		check_dir(line, count, cub);
+	else if (!(ft_strncmp(tmp[0], "C", 1)) || !(ft_strncmp(tmp[0], "F", 1)))
+		check_color(line, count, cub);
+	else
+		printf("unknown dir or color\n");
+	free_2d_arr(tmp, 0);
 }
 
-void	valid_map_check()
+int	is_valid_value(char *line, t_cub *cub)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(line))
+	{
+		if (line[i] == '0' || line[i] == '1'
+			|| line[i] == ' ' || line[i] == '\n')
+			continue ;
+		else if (line[i] == 'E' || line[i] == 'W'
+			|| line[i] == 'S' || line[i] == 'N')
+			cub->player_num++;
+		else
+			return (0);
+		i++;
+	}
+	retunr (1);
+}
+
+void	valid_map_check(char *line, char **map, t_cub *cub)
+{
+	char	*tmp;
+
+	tmp = *map;
+	if (is_valid_vlaue(line, cub))
+		*map = ft_strjoin(tmp, line, ft_strlen(line), ft_strlen(tmp));
+	else
+	{
+		free(line);
+		free(*map);
+		printf("invalid value in map\n");
+	}
 }
